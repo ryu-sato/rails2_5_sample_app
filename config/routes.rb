66d1a698459af29de0e4724f21796f7c526cd961
Ryu Sato <ryu@weseek.co.jp>
2018-10-21 13:29:33 +0900

    initialize activeadmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: 'tickets#index'
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
+  root to: 'tickets#index'
 
   resources :tickets, only: [:index, :show, :new, :create]
   resources :comparison_sets, only: [:show]
