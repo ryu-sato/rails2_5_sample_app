11a4901a6ae0211a3e51a7a08e9c53dcb544746e
Ryu Sato <ryu@weseek.co.jp>
2019-02-12 02:05:02 +0900

    basic 認証を設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :login
+
+  private
+
+    # Basic認証を行う
+    def login
+      authenticate_or_request_with_http_basic('authentication') do |username, password|
+        username == basic_auth_username && Digest::SHA1.hexdigest(password) == basic_auth_password
+      end
+    end
+    
+    def basic_auth_username
+      'test'
+    end
+
+    def basic_auth_password
+      '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8' # hash of 'password'
+    end
 end
